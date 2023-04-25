84a3b82032952ae93ebe4dada8326176075a4efd
Pradeep <iampradeephr@gmail.com>
2023-04-26 00:30:05 +0530

    manual image slider

--- lib/widgets/carousel_slider_raw.dart
+++ lib/widgets/carousel_slider_raw.dart
@@ -12,6 +12,8 @@ class CarouselSlider extends StatefulWidget {
 class _CarouselSliderState extends State<CarouselSlider> {
   late final PageController _pageController;
 
+  int pageNo = 0;
+
   final List<String> images = [
     "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
     "https://images.unsplash.com/photo-1609250291996-fdebe6020a8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
@@ -23,10 +25,7 @@ class _CarouselSliderState extends State<CarouselSlider> {
 
   @override
   void initState() {
-    _pageController = PageController(
-      initialPage: 0,
-      viewportFraction: 0.85
-    );
+    _pageController = PageController(initialPage: 0, viewportFraction: 0.85);
     super.initState();
   }
 
@@ -45,6 +44,10 @@ class _CarouselSliderState extends State<CarouselSlider> {
             SizedBox(
               height: 200,
               child: PageView.builder(
+                onPageChanged: (value) {
+                  pageNo = value;
+                  setState(() {});
+                },
                 controller: _pageController,
                 itemBuilder: (context, index) {
                   return AnimatedBuilder(
@@ -54,29 +57,36 @@ class _CarouselSliderState extends State<CarouselSlider> {
                     },
                     child: Container(
                       margin: const EdgeInsets.only(
-                        right: 4,
-                        left: 4,
-                        top: 36,
-                        bottom: 12
-                      ),
+                          right: 4, left: 4, top: 36, bottom: 12),
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(24.0),
-                        color: Colors.amberAccent,
-                      ),
+                          borderRadius: BorderRadius.circular(24.0),
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(images[index]))),
                     ),
                   );
                 },
                 itemCount: images.length,
               ),
             ),
-
-
-            
-
-
-
-
-
+            const SizedBox(
+              height: 12.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                  images.length,
+                  (index) => Container(
+                        margin: const EdgeInsets.all(2.0),
+                        child: Icon(
+                          Icons.circle,
+                          size: 12.0,
+                          color: pageNo == index
+                              ? Colors.indigoAccent
+                              : Colors.grey.shade300,
+                        ),
+                      )),
+            )
           ],
         ),
       ),
